16f620ce58eb59f51f246ec610f22dc72be7b021
BozeBro <ben.ozua@gmail.com>
2024-05-03 16:55:09 -0400

    sequential version of dinics

--- dinics-seq.cpp
+++ dinics-seq.cpp
@@ -1,4 +1,4 @@
-#include "dinics-seq.hpp"
+// #include "dinics-seq.hpp"
 // create aug graph
 
 // find a layered graph (BFS)
@@ -16,11 +16,9 @@ Be able to run BFS and DFS through it in a simple way
 #include <cassert>
 #include <chrono>
 #include <cstdio>
-#include <cstring>
 #include <fstream>
 #include <iostream>
 #include <limits>
-#include <omp.h>
 #include <ostream>
 #include <queue>
 #include <stack>
@@ -28,7 +26,55 @@ Be able to run BFS and DFS through it in a simple way
 #include <unistd.h>
 #include <unordered_map>
 #include <vector>
+#define UNSET std::numeric_limits<int>::max()
+#define SOURCE 0
+#define SINK 1
+struct Edge {
+  int cap;
+  int initial_cap;
+};
+struct Vertex {
+  int index;
+  int current_edge = 0;
+  int layer = UNSET;
+  int parent = -1;
+  std::vector<int> layered_dst;
 
+  void reset() {
+    this->current_edge = 0;
+    this->layer = UNSET;
+    this->parent = -1;
+    this->layered_dst.clear();
+  }
+};
+namespace std {
+template <> struct hash<Vertex> {
+  std::size_t operator()(const Vertex &obj) const {
+    // index is the unique identifier anyway
+    return std::hash<int>()(obj.index);
+  }
+};
+} // namespace std
+
+struct Graph {
+
+  std::vector<Vertex> vertices;
+  std::vector<std::unordered_map<int, Edge>> neighbors;
+  Graph(int size);
+  void printEdges();
+  void printEdgesVisualized();
+  void validate();
+  bool bfsParallel();
+  bool isLayerReachable(const Vertex &srcVert, const Vertex &dstVert);
+  bool visitVertexParallel(Vertex &srcVert, Vertex &dstVert);
+  bool visitVertex(Vertex &srcVert, Vertex &dstVert);
+  bool bfs();
+  void increment(int node);
+  bool dfsDeadEdge();
+  bool dfs();
+  void addEdge(const Vertex &start, const Vertex &end, int cap);
+  void reset();
+};
 #define UNSET std::numeric_limits<int>::max()
 #define SOURCE 0
 #define SINK 1
@@ -373,7 +419,6 @@ int main(int argc, char **argv) {
   // dst: 1
   // nodes 2,3
   // src -5-> [2,3] -5-> dstVert
-  omp_set_num_threads(1);
   const auto init_start = std::chrono::steady_clock::now();
   std::string input_filename;
   char mode = '\0';
